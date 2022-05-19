ad61192000245e0f0f3aa757271347e61eb2227b
Fabio Gomes <fabiolawliet@gmail.com>
2022-05-19 19:24:55 -0300

    Primeiro Commit

---
+++ Models/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SysPatrimonio.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> opcoes) : base(opcoes) { }
+
+        public DbSet<DbUsuario> usuarios { get; set; }
+        public DbSet<DbCategoria> categoria { get; set; }
+        public DbSet<DbFornecedor> fornecedor { get; set; }
+        public DbSet<DbPatrimonio> patrimonio { get; set; }
+        public DbSet<DbDepartamento> departamento { get; set; }
+        public DbSet<DbLocal> local { get; set; }
+
+    }
+}
---
+++ Models/DbPatrimonio.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace SysPatrimonio.Models
+{
+    [Table("patrimonio", Schema = "public")]
+    public class DbPatrimonio
+    {
+        [Key]
+
+        public int id { get; set; }
+        public string numetiqueta { get; set; }
+        public string nomepatrimonio { get; set; }
+        public string descricaopatrimonio { get; set; }
+        public decimal valorpatrimonio { get; set; }
+        public int idcategoria { get; set; }
+        public int idlocal {get; set;}
+        public int iddepartamento { get; set; }
+        public string marcamodelo { get; set }
+        public DateOnly dataaquisicao { get; set; }
+        public DateOnly databaixa { get; set; }
+        public int numnf { get; set; }
+        public string numserie { get; set; }
+        public string situacao { get; set; }
+        public int idfornecedor { get; set; }
+        public DateOnly datagarantia { get; set; }
+
+    }
+}
